bb93570f2866255c3ce0cea836c1164ddff8d94e
rds668 <delgadoshepherdrafael@gmail.com>
2023-12-11 11:04:42 +0100

    first commit

---
+++ src/main/java/cesur/examen/core/common/FileUtils.java
@@ -0,0 +1,41 @@
+package cesur.examen.core.common;
+
+import cesur.examen.core.worker.Worker;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * EXAMEN DE ACCESO A DATOS
+ * Diciembre 2023
+ *
+ * Nombre del alumno: Rafael Delgado Shepherd
+ * Fecha: 11/12/2023
+ *
+ * No se permite escribir en consola desde las clases DAO, Service y Utils usando System.out.
+ * En su lugar, usa log.info(), log.warning() y log.severe() para mostrar información interna
+ * o para seguir la traza de ejecución.
+ */
+
+
+public class FileUtils {
+
+    public static void toCSV(String fileName, List<Worker> workers) {
+
+
+        /*
+        Uncomment and implement body method!...
+        */
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (Worker worker : workers) {
+                // Formato CSV: id, name, dni, from
+                String line = String.format("%d,%s,%s,%tF%n", worker.getId(), worker.getName(), worker.getDni(), worker.getFrom());
+                writer.write(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/cesur/examen/core/worker/WorkerService.java
@@ -0,0 +1,59 @@
+package cesur.examen.core.worker;
+
+import java.util.Date;
+
+/**
+ * EXAMEN DE ACCESO A DATOS
+ * Diciembre 2023
+ *
+ * Nombre del alumno: Rafael Delgado Shepherd
+ * Fecha: 11/12/2023
+ *
+ *  No se permite escribir en consola desde las clases DAO, Service y Utils usando System.out.
+ *  En su lugar, usa log.info(), log.warning() y log.severe() para mostrar información interna
+ *  o para seguir la traza de ejecución.
+ */
+/**
+ *  Services classes offers methods to our main application, and can
+ *  use multiple methods from DAOs and Entities.
+ *  It's a layer between Data Access Layer and Aplication Layer (where
+ *  Main app and controllers lives)
+ *  It helps to encapsulate multiple opperations with DAOs that can be
+ *  reused in application layer.
+ */
+public class WorkerService {
+    /*
+    RenovateWorker() set "from" date of the worker with this dni to today's date.
+    Remember Date().
+    Returns the new updated worker, null if fails or dni doesn't exist.
+    */
+
+    private static final WorkerDAO workerDAO = new WorkerDAO();
+    public static Worker renovateWorker(String dni){
+        Worker workerUpdate = workerDAO.getWorkerByDNI(dni);
+
+        if(workerUpdate != null){
+            //Obtener fecha actual
+            Date currentDate = new Date(System.currentTimeMillis());
+
+            //Actualizar el campo from
+            workerUpdate.setFrom(currentDate);
+
+            // Actualizar el trabajador en la base de datos
+            Worker updatedWorker = workerDAO.update(workerUpdate);
+
+            if (updatedWorker != null) {
+                return updatedWorker;
+            } else {
+                // La actualización falló
+                return null;
+            }
+        } else {
+            // El trabajador con el DNI dado no existe
+            return null;
+        }
+
+        }
+
+
+}
